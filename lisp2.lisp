ab304497568a43ac65ace0d476337009b545ac80
feola <feolan@mail.ru>
2012-07-29 18:32:50 +0400

    29.07.12 add lisp2

---
+++ lisp2.lisp
@@ -0,0 +1,8 @@
+
+(defun golova (x y) (car sp))
+(defun hvost (x y) (cdr sp))
+(defvar sp (list x y))
+
+; Как сделать, чтобы написав, например, > (golova 2 3)
+; или > (hvost 2 3)  получить числа, а не x и (y)?
+; Не решено.
