31d08f3cd099940a3cb9770dc1a924788a2822a6
feola <feolan@mail.ru>
2012-11-22 23:56:47 +0400

    add test

--- macro1.lisp
+++ macro1.lisp
@@ -1,4 +1,11 @@
-;; Сделать макрос, который создаёт функцию.
+;; Сделать простой макрос
+
+(defmacro test (x)
+  (cons '+ x))
+
+(test (2 3 4))
+
+;; Сделать макрос, который создаёт функцию
 
 (defmacro macrosumma (x y)
   '(defun summa (x y) (+ x y)))
@@ -6,3 +13,7 @@
 (macrosumma x y)
 
 (macroexpand-1 '(macrosumma x y))
+
+;; Сделать макрос, который создаёт функцию с любым именем
+
+(defmacro
