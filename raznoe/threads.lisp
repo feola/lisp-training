f4b84fe6f113ccfcdc26abb56abf2f11a9f236e5
feola <feolan@mail.ru>
2013-02-26 18:36:35 +0400

    with-lock

--- raznoe/threads.lisp
+++ raznoe/threads.lisp
@@ -5,6 +5,26 @@
 (defparameter *i* 0)
 (defparameter *lock* (make-lock "my-lock"))
 
+(defmacro with-lock (name)
+  `(defun my-print (name)
+     (acquire-lock *lock* t)
+     (format *so* "~%~A: ~A" ,name *i*)
+     (release-lock *lock*)))
+
+(with-lock "a")
+;; Раскрывается в
+;; => (PROGN
+;;      (EVAL-WHEN (:COMPILE-TOPLEVEL) (SB-C:%COMPILER-DEFUN 'MY-PRINT 'NIL T))
+;;      (EVAL-WHEN (:LOAD-TOPLEVEL :EXECUTE)
+;;        (SB-IMPL::%DEFUN 'MY-PRINT
+;;                         (SB-INT:NAMED-LAMBDA MY-PRINT
+;;                             (NAME)
+;;                           (BLOCK MY-PRINT
+;;                             (ACQUIRE-LOCK *LOCK* T)
+;;                             (FORMAT *SO* "~%~A: ~A" "a" *I*)
+;;                             (RELEASE-LOCK *LOCK*)))
+;;                         NIL 'NIL (SB-C:SOURCE-LOCATION)))), T
+
 (defun thread-a ()
     (do ((n))
               ((> *i* 1000000) (print "a wins!" *so*))
@@ -17,13 +37,8 @@
           (my-print "b")
           (decf *i*)))
 
-(defun my-print (name)
-    (acquire-lock *lock* t)
-      (format *so* "~%~A: ~A" name *i*)
-        (release-lock *lock*))
-
 (progn
-    (make-thread #'thread-b :name "b")
-      (make-thread #'thread-a :name "a"))
+  (make-thread #'thread-b :name "b")
+  (make-thread #'thread-a :name "a"))
 
 (all-threads)
