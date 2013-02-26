8acd656a885dfd4f6ca0256598b80403745b26fa
feola <feolan@mail.ru>
2013-02-26 21:15:50 +0400

    modif with-lock

--- raznoe/threads.lisp
+++ raznoe/threads.lisp
@@ -5,13 +5,13 @@
 (defparameter *i* 0)
 (defparameter *lock* (make-lock "my-lock"))
 
-(defmacro with-lock (name)
+(defmacro with-lock (name &body body)
   `(defun my-print (name)
      (acquire-lock *lock* t)
-     (format *so* "~%~A: ~A" ,name *i*)
+     ,body
      (release-lock *lock*)))
 
-(with-lock "a")
+(with-lock "a" format *so* "~%~A: ~A" name *i*)
 ;; Раскрывается в
 ;; => (PROGN
 ;;      (EVAL-WHEN (:COMPILE-TOPLEVEL) (SB-C:%COMPILER-DEFUN 'MY-PRINT 'NIL T))
@@ -21,9 +21,9 @@
 ;;                             (NAME)
 ;;                           (BLOCK MY-PRINT
 ;;                             (ACQUIRE-LOCK *LOCK* T)
-;;                             (FORMAT *SO* "~%~A: ~A" "a" *I*)
+;;                             (FORMAT *SO* "~%~A: ~A" NAME *I*)
 ;;                             (RELEASE-LOCK *LOCK*)))
-;;                         NIL 'NIL (SB-C:SOURCE-LOCATION)))), T
+;;                         NIL 'NIL (SB-C:SOURCE-LOCATION))))
 
 (defun thread-a ()
     (do ((n))
